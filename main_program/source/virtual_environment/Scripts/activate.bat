0d8d640711a78ad1d3e0fbd6be0bf7c4aa5d6af8
KhodeNima <khodenimacollab@gmail.com>
2024-01-05 18:43:12 +0330

    Main .gitattributes
    LFS:
    
        1. Initialized Git Large File System logic for launcher files

--- main_program/source/virtual_environment/Scripts/activate.bat
+++ main_program/source/virtual_environment/Scripts/activate.bat
@@ -1,3 +1,33 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:3573b6629d03dd09f6d4b2edf2348b8285016fba63f82d60c9a6450070f85ce0
-size 1011
+@echo off
+
+rem This file is UTF-8 encoded, so we need to update the current code page while executing it
+for /f "tokens=2 delims=:." %%a in ('"%SystemRoot%\System32\chcp.com"') do (
+    set _OLD_CODEPAGE=%%a
+)
+if defined _OLD_CODEPAGE (
+    "%SystemRoot%\System32\chcp.com" 65001 > nul
+)
+
+set VIRTUAL_ENV=C:\Users\dell\Desktop\projects\nyvonethunter_project\main_program\source\virtual_environment
+
+if not defined PROMPT set PROMPT=$P$G
+
+if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
+if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
+
+set _OLD_VIRTUAL_PROMPT=%PROMPT%
+set PROMPT=(virtual_environment) %PROMPT%
+
+if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
+set PYTHONHOME=
+
+if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
+if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
+
+set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
+
+:END
+if defined _OLD_CODEPAGE (
+    "%SystemRoot%\System32\chcp.com" %_OLD_CODEPAGE% > nul
+    set _OLD_CODEPAGE=
+)
--- main_program/source/virtual_environment/Scripts/deactivate.bat
+++ main_program/source/virtual_environment/Scripts/deactivate.bat
@@ -1,3 +1,21 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:27991906d4fa2c4a2ff3235765dd67c63b4c29d2eed49875612d42be3681b7dc
-size 347
+@echo off
+
+if defined _OLD_VIRTUAL_PROMPT (
+    set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
+)
+set _OLD_VIRTUAL_PROMPT=
+
+if defined _OLD_VIRTUAL_PYTHONHOME (
+    set "PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%"
+    set _OLD_VIRTUAL_PYTHONHOME=
+)
+
+if defined _OLD_VIRTUAL_PATH (
+    set "PATH=%_OLD_VIRTUAL_PATH%"
+)
+
+set _OLD_VIRTUAL_PATH=
+
+set VIRTUAL_ENV=
+
+:END
